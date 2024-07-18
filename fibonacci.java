8daeebf59eae884a09d20950831ff7a6d5db15e7
Mr-Introvert <71131567+Mr-Introvert@users.noreply.github.com>
2024-07-18 21:36:03 +0530

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,18 +1,23 @@
-package JAVA;
 import java.util.Scanner;
-
-public class fibonacci {
+public class FibonacciSequence{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
         int num = sc.nextInt();
         int prev = 0, cur = 1, next = 0;
-
-        for(int i=0;i<num;i++){
-            System.out.print(next);
-            next = prev + cur;
-            prev = cur;
-            cur = next;
+        if(num<0){
+            System.out.println("Invalid Input");
         }
+        else{
+            for(int i= 0; i<num;i++){
+                System.out.println(next);
+                prev = cur;
+                cur = next;
+                next = prev + cur;
+            
+            }
+            System.out.printf("Here are %d numbers from the fibonacci sequence",num);
+        }
+        sc.close();
     }
-    
 }
