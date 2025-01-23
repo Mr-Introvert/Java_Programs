26062f44464c3e10b1ac85251ff58ae0b31ae6ab
Mr-Introvert <71131567+Mr-Introvert@users.noreply.github.com>
2025-01-23 12:47:41 +0530

    Add files via upload

---
+++ Array2.java
@@ -0,0 +1,33 @@
+package Array2;
+public class Array2 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+
+        int[] arr3 =new int[n1+n2];
+        int k= 0;
+
+        for(int i=0; i<n1; i++){
+            for(int j=0; j<n2; j++){
+                if(arr1[i] <= arr2[i]){
+                    arr3[k] = arr1[i];
+                    k++;
+                    break;
+                }
+                else{
+                    arr3[k] = arr2[i];
+                    k++;
+                    break;
+                }
+            }
+        }
+        System.out.print("Merged sorted array: ");
+        for (int p=0; p<=arr3; p++) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
